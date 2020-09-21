2c02d143ae96a2e765dc98cdccc5e1c620d46b50
Monk <42776976+Monk84@users.noreply.github.com>
2020-09-21 15:50:56 +0300

    Create main.cpp

---
+++ 1_Simple_Recognizer/main.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                              Laboratory work 1
+Student: Amir Kiamov
+Variant: 23
+Task: develop simple recognizer of ed2k file links based on: 
+1) regex 
+2) finite state machine made with SMC
+
+Make CLI interface with both file and console input possibility, after
+execution output statistics for correct lines based on hash value into both file 
+or console.
+
+Syntax: sr [-t [fsm|reg]] [-i [console|file [filename]]] [-o [console|file [filename]]]
+-t - type of solution (default fsm)
+reg = RegEx
+fsm = finite state machine
+-i(o) - input (output) type (default console)
+console - console input (output)
+file - file input (ouput)
+when choosing "file", specify filename by address
+*******************************************************************************/
+
+#include <iostream>
+#include "RegExSolution"
+
+using namespace std;
+
+int main( int argc,char* argv[] )
+{
+    
+    return 0;
+}
