13b248f664599cab62cc6aaef3354973c73b20ef
Monk <42776976+Monk84@users.noreply.github.com>
2020-09-21 10:51:44 +0300

    Create RegExSolution.h
    Regex solution, icase may not be in final version

---
+++ 1_Simple_Recognizer/RegExSolution.h
@@ -0,0 +1,12 @@
+#include <regex>
+#include <string>
+
+std::regex r( "^ed2k://\\|file\\|[\\w.+-]+\\|[\\d]+\\|[0-9a-f]+\\|/$",
+    std::regex_constants::icase );
+//^ - beginning of string, $ - end of string, \w = [a-zA-Z0-9_],
+//\d = [0-9],icase == case insensitive
+
+bool RegExSolution( const std::string& s )
+{
+    return std::regex_match( s,r );
+}
