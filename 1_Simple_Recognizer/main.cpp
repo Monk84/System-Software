53dcfa0f6af65e9c54bdc9a9a3596a44df185248
Monk <42776976+Monk84@users.noreply.github.com>
2020-09-23 18:02:32 +0300

    Update main.cpp

--- 1_Simple_Recognizer/main.cpp
+++ 1_Simple_Recognizer/main.cpp
@@ -28,20 +28,156 @@ when choosing "file", specify filename by address
 
 using namespace std;
 
+std::string GetHash( const std::string& line )
+{
+    //assumed that string is valid, behavior is undefined for invalid string
+    std::regex r( "\\|[0-9a-f]+\\|/" );
+    std::smatch m;
+    std::regex_search( line,m,r );
+    if( m.length() < 1 )
+    {
+        return std::string( "" );
+    }
+    std::string result = m.str( 0 );
+    return result.substr( 1,result.size()-3 );
+}
+
 int main( int argc,char* argv[] )
 {
-  bool type_fsm = True;
-  bool type_input_console = True;
-  bool type_output_console = True;
-    if( argc > 1 )
-      switch( *(argv[1]+1) ):
-      case 't'
-      {
+    if( argc < 3 )
+    {
+        std::cout << "No arguments - starting with default arguments.\n";
+    }
+    bool type_fsm = true;
+    bool type_input_console = true;
+    bool type_output_console = true;
+    std::ifstream ifile;
+    std::ofstream ofile;
+    FSMSolution fsm;
+    
+    for( int i = 1;i < std::min(argc,9);++i ) // no more than 9 arguments
+    {
+        std::string arg = argv[ i ];
+        if( arg == "-t" )
+        {
+            if( i + 1 < argc )
+            {
+                std::string etype = argv[ i+1 ];
+                if( etype == "reg" )
+                {
+                    type_fsm = false;
+                }
+                else if( !(etype == "fsm") )
+                {
+                    std::cout << "Unrecognized execution type. Set to fsm.\n";
+                }
+                ++i;
+            }
+        }
+        else if( arg == "-i" )
+        {
+            if( i + 1 < argc )
+            {
+                std::string itype = argv[ i+1 ];
+                if( itype == "file" )
+                {
+                    if( i + 2 < argc )
+                    {
+                        ifile.open( argv[ i+2 ],std::ifstream::in );
+                        type_input_console = false;
+                        if( !ifile )
+                        {
+                            std::cout << "Bad filename. Input set to console.\n";
+                            type_input_console = true;
+                        }
+                        ++i;
+                    }
+                }
+                else if( !(itype == "console") )
+                {
+                    std::cout << "Unrecognized input type. Set to console.\n";
+                }
+                ++i;
+            }
+        }
+        else if( arg == "-o" )
+        {
+            if( i + 1 < argc )
+            {
+                std::string itype = argv[ i+1 ];
+                if( itype == "file" )
+                {
+                    if( i + 2 < argc )
+                    {
+                        ifile.open( argv[ i+2 ],std::ofstream::trunc );
+                        type_output_console = false;
+                        if( !ifile )
+                        {
+                            std::cout << "Bad filename. Output set to console.\n";
+                            type_output_console = true;
+                        }
+                        ++i;
+                    }
+                }
+                else if( !(itype == "console") )
+                {
+                    std::cout << "Unrecognized output type. Set to console.\n";
+                }
+                ++i;
+            }
         
-      }
-      if( *(argv[1]+1) == 't')
-        if( argv[2] && *argv[2] == '' )
-          type_fsm = False;
-      
+        }
+    }
+    
+    FSMSolution* fsmsol;
+    std::map<std::string,int> stats;
+    bool (*exec_func)( const std::string& s );
+    std::istream input( NULL );
+    std::ostream output( NULL );
+    if( type_fsm )
+    {
+        exec_func = &RegExSolution;
+    }
+    else
+    {
+        exec_func = &(fsmsol->CheckString);
+    }
+    if( type_input_console )
+    {
+        input.rdbuf( std::cin.rdbuf() );
+    }
+    else
+    {
+        input.rdbuf( ifile.rdbuf() );
+    }
+    if( type_output_console )
+    {
+        output.rdbuf( std::cout.rdbuf() );
+    }
+    else
+    {
+        output.rdbuf( ofile.rdbuf() );
+    }
+    
+    if( type_input_console )
+    {
+        std::cout << "Input line by line:\n";
+    }
+    
+    while( input.good() )
+    {
+        std::string line;
+        std::getline( input,line );
+        if( exec_func(line) )
+        {
+            std::string hash = GetHash( line );
+            ++stats[hash];
+        }
+    }
+    output << "Statistics:\nHash - number'o'occurencies";
+    for( auto pair : stats )
+    {
+        output << std::get<0>(pair) << " - " << std::get<1>(pair) << std::endl;
+    }
     return 0;
 }
