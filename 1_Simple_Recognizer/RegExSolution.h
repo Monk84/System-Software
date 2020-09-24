3ca4063f3bc5ad3bba7c63024d83337067b505e9
Monk <42776976+Monk84@users.noreply.github.com>
2020-09-24 20:00:04 +0300

    Update RegExSolution.h

--- 1_Simple_Recognizer/RegExSolution.h
+++ 1_Simple_Recognizer/RegExSolution.h
@@ -1,10 +1,8 @@
 #include <regex>
 #include <string>
 
-std::regex r( "^ed2k://\\|file\\|[\\w.+-]+\\|[\\d]+\\|[0-9a-f]+\\|/$",
-    std::regex_constants::icase );
-//^ - beginning of string, $ - end of string, \w = [a-zA-Z0-9_],
-//\d = [0-9],icase == case insensitive
+std::regex r( "^ed2k://\\|file\\|[\\w.+-]+\\|[\\d]+\\|[0-9a-fA-F]+\\|/$" );
+//^ - beginning of string, $ - end of string, \w = [a-zA-Z0-9_], \d = [0-9]
 
 bool RegExSolution( const std::string& s )
 {
