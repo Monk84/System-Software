719bb46f2ee9dce8a27c7954ae8f1ce3770ef657
Monk <42776976+Monk84@users.noreply.github.com>
2020-10-06 23:49:13 +0300

    Update FSMSolution.h

--- 1_Simple_Recognizer/FSMSolution.h
+++ 1_Simple_Recognizer/FSMSolution.h
@@ -1,39 +1,65 @@
-#include "FSMSolution_sm.h"
+#pragma once
+#include "recognizer_sm.h"
+#include <string.h>
 
-class FSMSolution : public FSMSOlutionContext<FSMSolution>
+class FSMSolution : public recognizerContext<FSMSolution>
 {
 private:
-	FSMSOlutionContext fsm;
-	bool acceptable;
+	recognizerContext fsm;
+	bool isAcceptable;
+	std::string buffer;
+	int counter;
+	const static std::string valid_begin;
 public:
-	FSMSolution()
-		:
-		acceptable(False)
-	{}
-	~FSMSolution()
-	{}
-	inline bool CheckString(const std::string& s)
-	{
-		if( s.size() > 13 && s.substr(0,12).compare("ed2k://|file|") == 0 )
-			for( char c : s.substr( 13,s.size()-1 )
-			{
-				try
-				{
-					fsm.symb(c);
-				}
-				catch( ... )
-				{
-					return false;
-				}
-			}
-		return acceptable;
-	}
+	inline FSMSolution() = default;
+	
+	~FSMSolution();
+
+	bool CheckString( const std::string& s );
+
 	inline void Acceptable()
 	{
-		acceptable = True;
+		isAcceptable = true;
 	}
+
 	inline void Unacceptable()
 	{
-		acceptable = False;
+		isAcceptable = false;
 	}
-}
+
+	inline void CounterTick()
+	{
+		counter += 1;
+	}
+
+	inline int CounterState() const
+	{
+		return counter;
+	}
+
+	inline void ResetCounter()
+	{
+		counter = 0;
+	}
+
+	inline void BufferAdd( char c )
+	{
+		buffer.push_back( c );
+	}
+
+	inline bool BufferValidStatic() const
+	{
+		std::size_t found = valid_begin.find( buffer );
+		return found != std::string::npos;
+	}
+
+	inline bool BufferFullStatic() const
+	{
+		return buffer == valid_begin;
+	}
+
+	inline void ResetBuffer()
+	{
+		buffer.clear();
+	}
+};
